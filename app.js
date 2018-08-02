bd3eb32a8c416bbe22f1270e37befa8deca43b13
Gittie Atlas <gittieatlas@gmail.com>
2018-08-02 18:00:03 -0400

    add add route

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const postRoutes = require('./routes/posts');
 const app = express();
 
-app.use(morgan("dev"));
-app.use('/posts', postRoutes)
-app.use(express.static(__dirname + "/public"));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/posts', postRoutes);
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.redirect('/posts')
-})
+  res.redirect('/posts');
+});
 
 const PORT = 4000;
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,26 +1,55 @@
 const express = require('express');
-const client = require("../db");
-const postList = require("../views/postList");
-const postDetails = require("../views/postDetails");
+const client = require('../db');
+const postList = require('../views/postList');
+const postDetails = require('../views/postDetails');
+const addPost = require('../views/addPost');
 
 const router = express.Router();
-const baseQuery = "SELECT posts.*, users.name, counting.upvotes FROM posts INNER JOIN users ON users.id = posts.userId LEFT JOIN (SELECT postId, COUNT(*) as upvotes FROM upvotes GROUP BY postId) AS counting ON posts.id = counting.postId\n";
+const baseQuery =
+  'SELECT posts.*, users.name, counting.upvotes FROM posts INNER JOIN users ON users.id = posts.userId LEFT JOIN (SELECT postId, COUNT(*) as upvotes FROM upvotes GROUP BY postId) AS counting ON posts.id = counting.postId\n';
 
+router.get('/', async (req, res, next) => {
+  console.log('in /');
 
-router.get("/", async (req, res, next) => {
   try {
     const data = await client.query(baseQuery);
     res.send(postList(data.rows));
-  } catch (error) { next(error) }
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get("/:id", async (req, res, next) => {
-	console.log('postid test')
+// DONT MOVE THIS OUT OF ORDER
+router.get('/add', (req, res) => {
+  console.log('in /add');
+  console.log(req.body);
+
+  res.send(addPost());
+});
+
+router.get('/:id', async (req, res, next) => {
+  console.log('in /id');
   try {
-    const data = await client.query(baseQuery + "WHERE posts.id = $1", [req.params.id]);
+    const data = await client.query(baseQuery + 'WHERE posts.id = $1', [
+      req.params.id
+    ]);
     const post = data.rows[0];
     res.send(postDetails(post));
-  } catch (error) { next(error) }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res) => {
+  const name = req.body.name;
+  const title = req.body.title;
+  const content = req.body.content;
+
+  // Insert the post in the database
+
+  // 'INSERT INTO posts (userid, title, content) VALUES ('14', 'test', 'content')
+
+  res.redirect(`/posts/${postId}`); // Redirect to the post details page
 });
 
 module.exports = router;
