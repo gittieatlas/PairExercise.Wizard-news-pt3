2be07edf09bbb02ad35656cd252fe71f02066b70
Gittie Atlas <gittieatlas@gmail.com>
2018-08-02 23:57:36 -0400

    review video refactor

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const postRoutes = require('./routes/posts');
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/posts', postRoutes);
+app.use('/posts', require('./routes/posts'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,10 @@
-const express = require('express');
+const router = require('express').Router();
 const client = require('../db');
 const SQL = require('sql-template-strings');
 const postList = require('../views/postList');
 const postDetails = require('../views/postDetails');
 const addPost = require('../views/addPost');
 
-const router = express.Router();
 const baseQuery =
   'SELECT posts.*, users.name, counting.upvotes FROM posts INNER JOIN users ON users.id = posts.userId LEFT JOIN (SELECT postId, COUNT(*) as upvotes FROM upvotes GROUP BY postId) AS counting ON posts.id = counting.postId\n';
 
@@ -49,14 +48,29 @@ router.post('/', async (req, res, next) => {
   const content = req.body.content;
 
   try {
-    const data = await client.query(
+    let userData = await client.query(
+      'SELECT * FROM users WHERE users.name = $1',
+      [name]
+    );
+    if (!userData.rows.length) {
+      userData = await client.query(
+        'INSERT INTO users (name) VALUES  ($1) RETURNING *',
+        [name]
+      );
+    }
+
+    const userId = userData.rows[0].id;
+    const postData = await client.query(
       SQL`INSERT INTO posts (userid, title, content)
-          VALUES (  (SELECT id FROM users WHERE name = ${name} ),
+          VALUES (  ${userId},
                     ${title},
                     ${content} )
-          RETURNING id`
+          RETURNING *`
     );
-    const postId = data.rows[0].id;
+    const postId = postData.rows[0].id;
+    const upvotesData = await client.query(
+      SQL`INSERT INTO upvotes (userId, postId) VALUES (${userId}, ${postId}) RETURNING *`
+    );
     res.redirect(`/posts/${postId}`);
   } catch (error) {
     next(error);
